adf0db9333892e0ae074acde56b5097a7a0a11c4
Rajko Vukovic <rajkovukovic@gmail.com>
2021-12-28 12:21:35 +0100

    improve display of AsyncValue

--- example/src/watch/EventDetailsView/EventDetailsView.svelte
+++ example/src/watch/EventDetailsView/EventDetailsView.svelte
@@ -60,7 +60,7 @@
 								{:else if !$showEventDetailsStream}
 									{#if event.type === AVStreamEventType.error || event.type === AVStreamEventType.avError}
 										<span style="color: red;">({event.type})</span>
-									{:else if event.type === AVStreamEventType.complete || event.type === AVStreamEventType.pending}
+									{:else if event.type === AVStreamEventType.complete || event.type === AVStreamEventType.avPending}
 										<span>({event.type})</span>
 									{/if}
 								{/if}
--- example/src/watch/EventDetailsView/EventTree.svelte
+++ example/src/watch/EventDetailsView/EventTree.svelte
@@ -1,24 +1,56 @@
 <script lang="ts">
 	import type { AVStreamEvent } from '../AVWatch/AVWatch';
+	import { AVStreamEventType } from '../AVWatch/AVWatch';
 
 	import JSONTree from '../json-tree/Root.svelte';
 
 	export let event: AVStreamEvent;
 	export let showDetails = false;
-</script>
 
-{#if event}
-	<JSONTree
-		value={showDetails
-			? {
+	function getRelevantData(event: AVStreamEvent) {
+		switch (event?.type) {
+			case AVStreamEventType.avPending:
+				return event.data.value;
+			case AVStreamEventType.avError:
+				return event.data.error;
+			default:
+				return undefined;
+		}
+	}
+
+	function getDisplayInfo(event: AVStreamEvent) {
+		switch (event?.type) {
+			case AVStreamEventType.avPending:
+			case AVStreamEventType.complete:
+				return {
+					id: event.id,
+					ordinal: event.ordinal,
+					timestamp: new Date(event.timestamp),
+					type: event.type
+				};
+			case AVStreamEventType.avValue:
+			case AVStreamEventType.avError:
+				return {
+					id: event.id,
+					ordinal: event.ordinal,
+					timestamp: new Date(event.timestamp),
+					type: event.type,
+					data: event.data.error ?? event.data.value
+				};
+			default:
+				return {
 					id: event.id,
 					ordinal: event.ordinal,
 					timestamp: new Date(event.timestamp),
 					type: event.type,
 					data: event.data
-			  }
-			: event.data}
-	/>
+				};
+		}
+	}
+</script>
+
+{#if event}
+	<JSONTree value={showDetails ? getDisplayInfo(event) : event.data} />
 {/if}
 
 <style>
