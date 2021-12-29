683fea296fa9260dbce84dadd5c80df5222bd7b2
Rajko Vukovic <rajkovukovic@gmail.com>
2021-12-29 19:24:28 +0100

    make build generate d.ts files again

--- rollup.config.js
+++ rollup.config.js
@@ -5,77 +5,72 @@ import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 import preprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
-// import typescript from "rollup-plugin-typescript2"
+import rollupTypescript from '@rollup/plugin-typescript';
+import typescript2 from "rollup-plugin-typescript2"
 
 // const devMode = false
 
 const tsconfig = {
-	allowJs: true,
-	baseUrl: '.',
-	rootDir: 'src/lib',
-	outDir: 'dist',
-	declaration: true,
-	declarationDir: 'dist',
 	transpileOnly: true,
-	paths: {
-		$lib: ['src/lib'],
-		'$lib/*': ['src/lib/*']
-	}
 };
 
-export default {
-	input: 'src/lib/index.ts',
-	treeshake: { moduleSideEffects: false },
-
-	output: [
-		{ file: `dist/${pkg.module}`, format: 'es', sourcemap: true },
-		{ file: `dist/${pkg.main}`, format: 'umd', name: pkg.name, sourcemap: true }
-	],
-	plugins: [
-		svelte({
-			emitCss: false,
-			preprocess: preprocess({
-				typescript: tsconfig
-			})
-		}),
-
-		// We don't need to alias when importing from 'svelthree-three'
-		// see  '"svelte": "src/Three.js"' in svelthree-three's package.json
-		/*
-        alias({
-            entries: [
-                {
-          //import from three source on build
-                    find: "three",
-                    replacement: __dirname + "/node_modules/svelthree-three/src/Three"
-                }
-            ]
-    }),
-    */
-
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-		typescript({ tsconfig: 'tsconfig.build.json' }),
-
-		terser({
-			output: {
-				comments: function (node, comment) {
-					var text = comment.value;
-					var type = comment.type;
-					if (type == 'comment2') {
-						// multiline comment
-						return /@licence/i.test(text);
-					}
-				}
-			},
-			mangle: false,
-			compress: {
-				pure_funcs: ['console.log', 'console.info']
-			}
+const beforeTypescriptPlugins = [
+	svelte({
+		emitCss: false,
+		preprocess: preprocess({
+			typescript: tsconfig
 		})
-	]
-};
+	}),
+	resolve({
+		browser: true,
+		dedupe: ['svelte']
+	}),
+	commonjs(),
+]
+
+const afterTypescriptPlugins = [
+	terser({
+		output: {
+			comments: function (node, comment) {
+				var text = comment.value;
+				var type = comment.type;
+				if (type == 'comment2') {
+					// multiline comment
+					return /@licence/i.test(text);
+				}
+			}
+		},
+		mangle: false,
+		compress: {
+			pure_funcs: ['console.log', 'console.info']
+		}
+	})]
+
+export default [
+	// use typescript2 for building d.ts files
+	{
+		input: 'src/lib/index.ts',
+		treeshake: { moduleSideEffects: false },
+		output:
+			{ file: `dist/${pkg.module}`, format: 'es', sourcemap: true },
+		plugins: [
+			...beforeTypescriptPlugins,
+			typescript2({ tsconfig: 'tsconfig.build.json' }),
+			...afterTypescriptPlugins,
+		]
+	},
+	// use typescript for everything else
+	{
+		input: 'src/lib/index.ts',
+		treeshake: { moduleSideEffects: false },
+		output: [
+			{ file: `dist/${pkg.module}`, format: 'es', sourcemap: true },
+			{ file: `dist/${pkg.main}`, format: 'umd', name: pkg.name, sourcemap: true }
+		],
+		plugins: [
+			...beforeTypescriptPlugins,
+			rollupTypescript({ tsconfig: 'tsconfig.build.json' }),
+			...afterTypescriptPlugins,
+		]
+	}
+];
